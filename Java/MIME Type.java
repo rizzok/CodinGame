c6dd63dddc0035f06c82161aeda899949136eb34
rizzok <kevin.rizzo@live.fr>
2020-04-30 10:49:21 +0200

    MIME Type code improvement

--- Java/MIME Type.java
+++ Java/MIME Type.java
@@ -11,7 +11,7 @@ class Solution {
         int N = in.nextInt(); // Number of elements which make up the association table.
         int Q = in.nextInt(); // Number Q of file names to be analyzed.
         for (int i = 0; i < N; i++) {
-            String EXT = in.next(); // file extension
+            String EXT = (in.next()).toLowerCase(); // file extension
             String MT = in.next(); // MIME type.
             // assigning inputs in HashMap
             hmMime.put(EXT, MT);
@@ -27,13 +27,9 @@ class Solution {
                 extension = FNAME.substring(FNAME.lastIndexOf(".") + 1);
             }
             // show Mime Type corresponding to extension or "UNKNOWN" if not
-            if (!extension.equals("")) {
-                String mimeType = hmMime.get(extension);
-                if (mimeType != null && extension.length() <= 10) {
-                    System.out.println(mimeType);
-                } else {
-                    System.out.println("UNKNOWN");
-                }
+            String mimeType = hmMime.get(extension);
+            if (!extension.equals("") && mimeType != null && extension.length() <= 10) {
+                System.out.println(mimeType);
             } else {
                 System.out.println("UNKNOWN");
             }
