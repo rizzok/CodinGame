6f2cfa6fca4578e2e50699376e2af57b3e096c76
rizzok <kevin.rizzo@live.fr>
2020-04-30 10:41:28 +0200

    MIME Type code improvement

--- Java/MIME Type.java
+++ Java/MIME Type.java
@@ -3,37 +3,34 @@ package Java;
 import java.util.*;
 
 class Solution {
-
     public static void main(String args[]) {
-        // déclaration du HashMap
+        // declaration of HashMap
         Map<String, String> hmMime = new HashMap<>();
-        // saisie des entrées
+        // scanner declaration and inputs
         Scanner in = new Scanner(System.in);
         int N = in.nextInt(); // Number of elements which make up the association table.
         int Q = in.nextInt(); // Number Q of file names to be analyzed.
         for (int i = 0; i < N; i++) {
             String EXT = in.next(); // file extension
             String MT = in.next(); // MIME type.
-            // assignation des entrées dans le HashMap
+            // assigning inputs in HashMap
             hmMime.put(EXT, MT);
         }
         in.nextLine();
+        // analyze of all filenames
         for (int i = 0; i < Q; i++) {
-            // entrée des noms de fichiers
+            // filenames inputs
             String FNAME = (in.nextLine()).toLowerCase(); // One file name per line.
-            // récupération de l'extention
+            // get extension of file
             String extension = "";
             if (FNAME.contains(".")) {
                 extension = FNAME.substring(FNAME.lastIndexOf(".") + 1);
             }
-
+            // show Mime Type corresponding to extension or "UNKNOWN" if not
             if (!extension.equals("")) {
-                // affichage du MIME correspondant à l'extension
-                String rep = hmMime.get(extension);
-                if (rep != null && extension.length() <= 10) {
-                    System.err.println("FNAME = " + FNAME);
-                    System.err.println("extension = " + extension);
-                    System.out.println(rep);
+                String mimeType = hmMime.get(extension);
+                if (mimeType != null && extension.length() <= 10) {
+                    System.out.println(mimeType);
                 } else {
                     System.out.println("UNKNOWN");
                 }
@@ -41,6 +38,5 @@ class Solution {
                 System.out.println("UNKNOWN");
             }
         }
-
     }
 }
